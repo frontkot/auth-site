2d28091e32b5790f392c0e47f02953aa425890e4
Konstantin Front <frontkot@gmail.com>
2021-06-25 14:02:25 +0300

    add header structure, add icons, add manin content

---
+++ src/components/Image/Image.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import classes from './Image.module.scss';
+
+const Image = ({
+  className, src, alt
+}) => (
+  <img alt={alt} src={src} className={className}/>
+);
+
+
+export default Image;
