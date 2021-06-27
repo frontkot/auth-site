af77930df4c0546290f063682dd3696b97378dbb
Konstantin Front <frontkot@gmail.com>
2021-06-27 12:25:25 +0300

    add simple styles for modal window

--- src/components/ModalWindow/ModalWindow.component.jsx
+++ src/components/ModalWindow/ModalWindow.component.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import classes from './ModalWindow.module.scss';
 import EntryForm from '../EntryForm/EntryForm.component';
 import Button from '../Button/Button.component';
@@ -9,9 +9,9 @@ import { isOpenWindow } from '../../store/modalWindow/actions';
 const ModalWindow = ({
   header, isEntry
 }) => {
+  const { modalWindow, modalBlock, modalHeader, modalContent } = classes;
   const dispatch = useDispatch();
   const isOpen = useSelector(checkIsOpen);
-  const modal = useRef();
 
   const checkIsModalWindow = (isModal) => isModal ? null : dispatch(isOpenWindow(!isOpen));
 
@@ -21,17 +21,17 @@ const ModalWindow = ({
   }
 
   return (
-    <div className={classes} onClick={() => modal ? null : checkIsModalWindow(!isOpen)}>
-      <div className={classes} ref={modal}>
-        <h3 className={classes}>{header}</h3>
+    <div className={modalWindow}  onClick={() => checkIsModalWindow(!isOpen)}>
+      <div className={modalBlock}>
+        <h3 className={modalHeader}>{header}</h3>
         {
           isEntry
             ?
               <EntryForm />
             :
-              <div className={classes}>
-                <Button textContent='Yes' onClick={signOut} />
-                <Button textContent='No' onClick={() => checkIsModalWindow(!isOpen)}/>
+              <div className={modalContent}>
+                <Button className='modalButton' textContent='Yes' onClick={signOut} />
+                <Button className='modalButton' textContent='No' onClick={() => checkIsModalWindow(!isOpen)}/>
               </div>
         }
       </div>
